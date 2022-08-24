19fc90c911a2458a1e6379bbcf756e9d4d14589f
VinaykuMRT <105343595+VinaykuMRT@users.noreply.github.com>
2022-08-24 15:20:43 +0530

    Add files via upload

---
+++ ConsoleAppdemo/ConsoleAppdemo/Program.cs
@@ -0,0 +1,30 @@
+﻿class Myclass
+{
+    enum Grade { pass=60,Distinction=85};
+
+    public static void Main()
+    { 
+int empid;
+int mark;
+
+ Console.WriteLine("whats your empid?");
+empid = int.Parse(Console.ReadLine());
+Console.WriteLine("hello world!");
+Console.WriteLine($"thanks for giving emp id {empid}");
+
+Console.WriteLine("enter the tsql marks");
+mark = Convert.ToInt32(Console.ReadLine());
+if (mark >=(int) Grade.Distinction)
+{
+    Console.WriteLine("you are Distinction");
+}
+else if (mark >=(int) Grade.pass)
+{
+    Console.WriteLine("you are pass ");
+}
+else
+{
+    Console.WriteLine("you are fail");
+}
+}
+}
